7df9b816ff47b57ef75bc0739bd3088a6440e5c0
ZohRiDev <zaharnu@gmail.com>
2026-03-16 22:26:19 +0300

    Халява

--- maen.py
+++ maen.py
@@ -1,12 +1,13 @@
 
-num1 = 12
-
-while num1 > 0:
-    num1 -= 1
-    if num1 != 4:
-        print(num1)
+secret_number1 = 7 
+fer2 = 0
+while True:
+    gerne = int(input('Введите число: '))
+    fer2 = fer2 + 1
+    if gerne < secret_number1:
+        print('Мало мыслишь!')
+    elif gerne > secret_number1:
+        print('Убавь темп , дружок.')
     else:
-        continue
-else:
-    print('УСЛОВИЕ НЕ ВЫПОЛНЕНО!')    
-print('Конец')
+        print(f'Крассавчик!Ты отгадал число за {fer2} попыток.')
+        break
